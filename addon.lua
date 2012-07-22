1fd8a8eb736939f384fc2dd3f07413186cab2c10
Phanx <addons@phanx.net>
2012-07-22 09:59:17 +0000

    - TOC 40300 until 5.0 goes live - Vendor values will now be shown while the auction house or quest reward windows are visible, in addition to while the merchant window is visible. - Added an option to hide equipment sets - Added an option to hide "Heroic" tags - Added an option to hide "Reforged" tags - Added an option to hide level, reputation, and skill requirements - Added itIT and zhCN compatibility - Added deDE translations - Added zhCN translations from hydra0 on CurseForge

--- addon.lua
+++ addon.lua
@@ -16,12 +16,16 @@ local GAME_LOCALE = GetLocale()
 local settings = {
 	compactBonuses = true,
 	enchantColor = { 0, 0.8, 1 },
+	hideEquipmentSets = true,
+--	hideHeroic = false,
 	hideItemLevel = true,
 	hideMadeBy = true,
+--	hideRaidFinder = false,
+--	hideReforged = false,
 	hideRightClickBuy = true,
 	hideRightClickSocket = true,
-	hideSellValue = false,
-	hideSoulbound = false,
+--	hideSellValue = false,
+--	hideSoulbound = false,
 	customTooltips = {
 		"AtlasLootTooltip",		-- AtlasLoot
 		"AtlasQuestTooltip",	-- AtlasQuest
@@ -38,18 +42,12 @@ namespace.settings = settings
 
 if not namespace.L then namespace.L = { } end
 
-local L = setmetatable( namespace.L, { __index = function( t, k )
+local L = setmetatable(namespace.L, { __index = function(t, k)
 	if k == nil then return "" end
-	local v = tostring( k )
-	t[ k ] = v
+	local v = tostring(k)
+	t[k] = v
 	return v
-end } )
-
-L["^%d+ Armor$"]	 = "^" .. ARMOR_TEMPLATE:gsub( "%%d", "%d+" ) .. "$"
-L["^Chance on hit:"] = "^" .. ITEM_SPELL_TRIGGER_ONPROC
-L["^Item Level %d"]  = "^" .. ITEM_LEVEL:gsub( "%%d", "%d+" )
-L["^<Made by %S+>$"] = "^" .. ITEM_CREATED_BY:gsub( "%%s", ".+" ) .. "$"
-L["^Socket Bonus:"]  = "^" .. ITEM_SOCKET_BONUS:gsub( "%%s", "" ):trim()
+end })
 
 ------------------------------------------------------------------------
 
@@ -66,28 +64,30 @@ local stat_strings = namespace.strings
 
 ------------------------------------------------------------------------
 
-local stat_names = setmetatable( { }, { __index = function( t, k )
-	if type( k ) ~= "string" then return "" end
+local stat_names = setmetatable({ }, { __index = function(t, k)
+	if type(k) ~= "string" then
+		return ""
+	end
 	local v
-	if GAME_LOCALE:match( "^[de]" ) then
+	if GAME_LOCALE:match("^[de]") then
 		-- de, en: Capitalize each word.
-		for word in k:gmatch( "%S+" ) do
-			local i, c = 2, word:sub( 1, 1 )
+		for word in k:gmatch("%S+") do
+			local i, c = 2, word:sub(1, 1)
 			if c:byte() > 127 then
-				i, c = 3, word:sub( 1, 2 )
+				i, c = 3, word:sub(1, 2)
 			end
-			word = c:upper() .. word:sub( i )
-			v = v and ( v .. " " .. word ) or word
+			word = c:upper() .. word:sub(i)
+			v = v and (v .. " " .. word) or word
 		end
-	elseif GAME_LOCALE:match( "^[ef][sr]" ) then
-		-- es, fr: Lowercase everything.
+	elseif GAME_LOCALE:match("^[ef][sr]") then
+		-- es, fr, it, pt: Lowercase everything.
 		v = k:lower()
 	else
 		v = k
 	end
-	rawset( t, k, v )
+	rawset(t, k, v)
 	return v
-end } )
+end })
 
 namespace.names = stat_names
 
@@ -99,43 +99,63 @@ local ITEM_SOCKETABLE = ITEM_SOCKETABLE
 local ITEM_SOULBOUND = ITEM_SOULBOUND
 local ITEM_VENDOR_STACK_BUY = ITEM_VENDOR_STACK_BUY
 local RAID_FINDER = RAID_FINDER
+local REFORGED = REFORGED
 
-local cache = setmetatable( { }, { __mode = "kv" } ) -- weak table to enable garbage collection
+local S_ARMOR_TEMPLATE = "^" .. ARMOR_TEMPLATE:gsub("%%d", "%%d+") .. "$"
+local S_ITEM_CREATED_BY = "^" .. ITEM_CREATED_BY:gsub("%%s", ".+") .. "$"
+local S_ITEM_LEVEL = "^" .. ITEM_LEVEL:gsub("%%d", "%%d+") .. "$"
+local S_ITEM_MIN_LEVEL = ITEM_MIN_LEVEL:gsub("%%d", "%%d+")
+local S_ITEM_REQ_REPUTATION = ITEM_REQ_REPUTATION:gsub("%%s", ".+")
+local S_ITEM_REQ_SKILL = ITEM_REQ_SKILL:gsub("%%s", ".+")
+local S_ITEM_SOCKET_BONUS = "^" .. ITEM_SOCKET_BONUS:gsub("%%s", ""):trim()
+local S_ITEM_SPELL_TRIGGER_ONEQUIP = "^"..ITEM_SPELL_TRIGGER_ONEQUIP -- not used
+local S_ITEM_SPELL_TRIGGER_ONPROC = "^"..ITEM_SPELL_TRIGGER_ONPROC
+local S_ITEM_SPELL_TRIGGER_ONUSE = "^"..ITEM_SPELL_TRIGGER_ONUSE -- not used
 
-local function ReformatItemTooltip( tooltip )
+local cache = setmetatable({ }, { __mode = "kv" }) -- weak table to enable garbage collection
+
+local function ReformatItemTooltip(tooltip)
 	local tooltipName = tooltip:GetName()
 	for i = 2, tooltip:NumLines() do
-		local line = _G[ tooltipName .. "TextLeft" .. i ]
+		local line = _G[tooltipName .. "TextLeft" .. i]
 		local text = line:GetText()
-		if text and text ~= ITEM_HEROIC and text ~= ITEM_HEROIC_EPIC then
-			if ( text == ITEM_SOULBOUND and settings.hideSoulbound ) or ( text == RAID_FINDER and settings.hideRaidFinder ) or ( text == ITEM_SOCKETABLE and settings.hideRightClickSocket ) or ( text == ITEM_VENDOR_STACK_BUY and settings.hideRightClickBuy ) or ( settings.hideItemLevel and text:match( L["^Item Level %d"] ) ) or ( settings.hideMadeBy and text:match( L["^<Made by %S+>$"] ) ) then
-				line:SetText( "" )
-			else
+		if text then
+			if (text == ITEM_HEROIC and settings.hideHeroic)
+			or (text == ITEM_SOCKETABLE and settings.hideRightClickSocket)
+			or (text == ITEM_SOULBOUND and settings.hideSoulbound)
+			or (text == ITEM_VENDOR_STACK_BUY and settings.hideRightClickBuy)
+			or (text == REFORGED and settings.hideReforged)
+			or (text == RAID_FINDER and settings.hideRaidFinder)
+			or (settings.hideItemLevel and text:match(S_ITEM_LEVEL))
+			or (settings.hideMadeBy and text:match(S_ITEM_CREATED_BY))
+			or (settings.hideRequirements and (text:match(S_ITEM_MIN_LEVEL) or text:match(S_ITEM_REQ_REPUTATION) or text:match(S_ITEM_REQ_SKILL) or text:match(L["Enchantment Requires"]) or text:match(L["Socket Requires"]))) then
+				line:SetText("")
+			elseif not text:match("<") then
 				local r, g, b = line:GetTextColor()
-				if r > 0.05 or g < 0.95 or text:match( "^%a+:" ) or text:match( L["^Chance on hit:"] ) or text:match( L["^%d+ Armor$"] ) or text:match( L["^Socket Bonus:"] ) then
+				if r > 0.05 or g < 0.95 or text:match("^%a+:") or text:match(S_ITEM_SPELL_TRIGGER_ONPROC) or text:match(S_ARMOR_TEMPLATE) or text:match(S_ITEM_SOCKET_BONUS) then
 					if settings.compactBonuses then
-						if cache[ text ] then
-							line:SetText( cache[ text ] )
-							line:SetTextColor( 0, 1, 0 )
+						if cache[text] then
+							line:SetText(cache[text])
+							line:SetTextColor(0, 1, 0)
 						else
-							for i, pattern in ipairs( stat_patterns ) do
-								local stat, value = text:match( pattern )
+							for i, pattern in ipairs(stat_patterns) do
+								local stat, value = text:match(pattern)
 								if stat then
-									if tonumber( stat ) then
+									if tonumber(stat) then
 										stat, value = value, stat
 									end
-									local str = stat_strings and stat_strings[ i ] or "+%d %s"
-									local result = str:format( value, stat_names[ stat ] or stat )
-									cache[ text ] = result
-									line:SetText( result )
-									line:SetTextColor( 0, 1, 0 )
+									local str = stat_strings and stat_strings[i] or "+%d %s"
+									local result = str:format(value, stat_names[stat] or stat)
+									cache[text] = result
+									line:SetText(result)
+									line:SetTextColor(0, 1, 0)
 									break
 								end
 							end
 						end
 					end
 				else
-					line:SetTextColor( unpack( settings.enchantColor ) )
+					line:SetTextColor(unpack(settings.enchantColor))
 				end
 			end
 			tooltip:Show()
@@ -143,7 +163,7 @@ local function ReformatItemTooltip( tooltip )
 	end
 end
 
-for _, tooltip in pairs( {
+for _, tooltip in pairs({
 	"GameTooltip",
 	"ItemRefTooltip",
 	"ItemRefShoppingTooltip1",
@@ -155,19 +175,29 @@ for _, tooltip in pairs( {
 	"WorldMapCompareTooltip1",
 	"WorldMapCompareTooltip2",
 	"WorldMapCompareTooltip3",
-} ) do
-	if _G[ tooltip ] then
-		_G[ tooltip ]:HookScript( "OnTooltipSetItem", ReformatItemTooltip )
+}) do
+	if _G[tooltip] then
+		_G[tooltip]:HookScript("OnTooltipSetItem", ReformatItemTooltip)
 	end
 end
 
 ------------------------------------------------------------------------
 
+local showPriceFrames = {
+	"AuctionFrame",
+	"MerchantFrame",
+	"QuestRewardPanel",
+}
+
 local prehook = GameTooltip_OnTooltipAddMoney
 
-function GameTooltip_OnTooltipAddMoney( ... )
-	if settings.hideSellValue and not MerchantFrame:IsShown() then
-		return
+function GameTooltip_OnTooltipAddMoney(...)
+	if not settings.hideSellValue then
+		return prehook(...)
 	end
-	return prehook( ... )
+	for _, frame in pairs(showPriceFrames) do
+		if _G[frame]:IsShown() then
+			return prehook(...)
+		end
+	end
 end
--- options.lua
+++ options.lua
@@ -11,20 +11,20 @@ local ADDON_NAME, namespace = ...
 local settings = namespace.settings
 local L = namespace.L
 
-local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( ADDON_NAME )
+local panel = LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(ADDON_NAME)
 
-panel:RegisterEvent( "ADDON_LOADED" )
-panel:SetScript( "OnEvent", function( self, event, addon )
+panel:RegisterEvent("ADDON_LOADED")
+panel:SetScript("OnEvent", function(self, event, addon)
 	if addon ~= ADDON_NAME then return end
 
 	if ItemTooltipCleanerSettings then
-		for k, v in pairs( settings ) do
-			if type( v ) ~= type( ItemTooltipCleanerSettings[ k ] ) then
-				ItemTooltipCleanerSettings[ k ] = v
+		for k, v in pairs(settings) do
+			if type(v) ~= type(ItemTooltipCleanerSettings[k]) then
+				ItemTooltipCleanerSettings[k] = v
 			end
 		end
-		for k, v in pairs( ItemTooltipCleanerSettings ) do
-			settings[ k ] = v
+		for k, v in pairs(ItemTooltipCleanerSettings) do
+			settings[k] = v
 		end
 	end
 	ItemTooltipCleanerSettings = settings
@@ -32,99 +32,129 @@ panel:SetScript( "OnEvent", function( self, event, addon )
 	local stat_names = namespace.names
 	local stat_patterns = namespace.patterns
 --[[
-	for k, v in pairs( _G ) do
-		if type( v ) == "string" and v:match( "%%d" ) and k:match( "^ITEM_MOD" ) and not k:match( "_SHORT$" ) then
-			local str = v:gsub( "%%d", 13 )
-			for i, pattern in ipairs( stat_patterns ) do
-				local stat, amount = str:match( pattern:gsub( "^" .. ITEM_SPELL_TRIGGER_ONEQUIP, "" ):trim() )
+	for k, v in pairs(_G) do
+		if type(v) == "string" and v:match("%%d") and k:match("^ITEM_MOD") and not k:match("_SHORT$") then
+			local str = v:gsub("%%d", 13)
+			for i, pattern in ipairs(stat_patterns) do
+				local stat, amount = str:match(pattern:gsub("^" .. ITEM_SPELL_TRIGGER_ONEQUIP, ""):trim())
 				if stat and amount then
 					if stat == "13" then stat = amount end
-					stat_names[ stat ] = _G[ k .. "_SHORT" ]
+					stat_names[stat] = _G[k .. "_SHORT"]
 				end
 			end
 		end
 	end
 --]]
 	self:UnregisterAllEvents()
-	self:SetScript( "OnEvent", nil )
-end )
+	self:SetScript("OnEvent", nil)
+end)
 
-panel.runOnce = function( self )
-	local title, notes = LibStub( "PhanxConfig-Header" ).CreateHeader( self, ADDON_NAME, GetAddOnMetadata( ADDON_NAME, "Notes" ) )
+panel.runOnce = function(self)
+	local title, notes = LibStub("PhanxConfig-Header").CreateHeader(self, ADDON_NAME, GetAddOnMetadata(ADDON_NAME, "Notes"))
 
-	local colorEnchant = LibStub( "PhanxConfig-ColorPicker" ).CreateColorPicker( self, L["Enchantment color"] )
-	colorEnchant:SetPoint( "TOPLEFT", notes, "BOTTOMLEFT", 4, -12 )
-	colorEnchant:SetColor( unpack( settings.enchantColor ) )
+	local colorEnchant = LibStub("PhanxConfig-ColorPicker").CreateColorPicker(self, L["Enchantment color"])
+	colorEnchant:SetPoint("TOPLEFT", notes, "BOTTOMLEFT", 4, -12)
+	colorEnchant:SetColor(unpack(settings.enchantColor))
 	colorEnchant.GetColor = function()
-		return unpack( settings.enchantColor )
+		return unpack(settings.enchantColor)
 	end
-	colorEnchant.OnColorChanged = function( self, r, g, b )
-		settings.enchantColor[ 1 ] = r
-		settings.enchantColor[ 2 ] = g
-		settings.enchantColor[ 3 ] = b
-	end
-
-	local CreateCheckbox = LibStub( "PhanxConfig-Checkbox" ).CreateCheckbox
-	local function OnClick( self, checked )
-		settings[ self.key ] = checked
+	colorEnchant.OnColorChanged = function(self, r, g, b)
+		settings.enchantColor[1] = r
+		settings.enchantColor[2] = g
+		settings.enchantColor[3] = b
 	end
 
-	local checkBonus = CreateCheckbox( self, L["Compact equipment bonuses"] )
-	checkBonus:SetPoint( "TOPLEFT", colorEnchant, "BOTTOMLEFT", -3, -10 )
+	local CreateCheckbox = LibStub("PhanxConfig-Checkbox").CreateCheckbox
+	local function OnClick(self, checked)
+		settings[self.key] = checked
+	end
+
+	local checkBonus = CreateCheckbox(self, L["Compact equipment bonuses"])
+	checkBonus:SetPoint("TOPLEFT", colorEnchant, "BOTTOMLEFT", -3, -10)
 	checkBonus.OnClick = OnClick
 	checkBonus.key = "compactBonuses"
 
-	local checkILevel = CreateCheckbox( self, L["Hide item levels"] )
-	checkILevel:SetPoint( "TOPLEFT", checkBonus, "BOTTOMLEFT", 0, -8 )
+	local checkILevel = CreateCheckbox(self, L["Hide item levels"])
+	checkILevel:SetPoint("TOPLEFT", checkBonus, "BOTTOMLEFT", 0, -8)
 	checkILevel.OnClick = OnClick
 	checkILevel.key = "hideItemLevel"
 
-	local checkBuy = CreateCheckbox( self, L["Hide buying instructions"] )
-	checkBuy:SetPoint( "TOPLEFT", checkILevel, "BOTTOMLEFT", 0, -8 )
-	checkBuy.OnClick = OnClick
-	checkBuy.key = "hideRightClickBuy"
+	local checkEquipSets = CreateCheckbox(self, L["Hide equipment sets"])
+	checkEquipSets:SetPoint("TOPLEFT", checkILevel, "BOTTOMLEFT", 0, -8)
+	checkEquipSets.OnClick = function(self, checked)
+		if checked then
+			SetCVar("dontShowEquipmentSetsOnItems", 1)
+		else
+			SetCVar("dontShowEquipmentSetsOnItems", 0)
+		end
+	end
 
-	local checkSocket = CreateCheckbox( self, L["Hide socketing instructions"] )
-	checkSocket:SetPoint( "TOPLEFT", checkBuy, "BOTTOMLEFT", 0, -8 )
-	checkSocket.OnClick = OnClick
-	checkSocket.key = "hideRightClickSocket"
+	local checkHeroic = CreateCheckbox(self, string.format(L["Hide \"%s\" tags"], ITEM_HEROIC))
+	checkHeroic:SetPoint("TOPLEFT", checkEquipSets, "BOTTOMLEFT", 0, -8)
+	checkHeroic.OnClick = OnClick
+	checkHeroic.key = "hideHeroic"
 
-	local checkMadeBy = CreateCheckbox( self, L["Hide \"Made By\" tags"] )
-	checkMadeBy:SetPoint( "TOPLEFT", checkSocket, "BOTTOMLEFT", 0, -8 )
+	local checkMadeBy = CreateCheckbox(self, string.format(L["Hide \"%s\" tags"], L["Made by"]))
+	checkMadeBy:SetPoint("TOPLEFT", checkHeroic, "BOTTOMLEFT", 0, -8)
 	checkMadeBy.OnClick = OnClick
 	checkMadeBy.key = "hideMadeBy"
 
-	local checkRaidFinder = CreateCheckbox( self, L["Hide \"Raid Finder\" tags"] )
-	checkRaidFinder:SetPoint( "TOPLEFT", checkMadeBy, "BOTTOMLEFT", 0, -8 )
+	local checkRaidFinder = CreateCheckbox(self, string.format(L["Hide \"%s\" tags"], RAID_FINDER))
+	checkRaidFinder:SetPoint("TOPLEFT", checkMadeBy, "BOTTOMLEFT", 0, -8)
 	checkRaidFinder.OnClick = OnClick
 	checkRaidFinder.key = "hideRaidFinder"
 
-	local checkSoulbound = CreateCheckbox( self, L["Hide \"Soulbound\" lines"] )
-	checkSoulbound:SetPoint( "TOPLEFT", checkRaidFinder, "BOTTOMLEFT", 0, -8 )
+	local checkReforged = CreateCheckbox(self, string.format(L["Hide \"%s\" tags"], REFORGED))
+	checkReforged:SetPoint("TOPLEFT", checkRaidFinder, "BOTTOMLEFT", 0, -8)
+	checkReforged.OnClick = OnClick
+	checkReforged.key = "hideReforged"
+
+	local checkSoulbound = CreateCheckbox(self, string.format(L["Hide \"%s\" tags"], ITEM_SOULBOUND))
+	checkSoulbound:SetPoint("TOPLEFT", checkReforged, "BOTTOMLEFT", 0, -8)
 	checkSoulbound.OnClick = OnClick
 	checkSoulbound.key = "hideSoulbound"
 
-	local checkValue = CreateCheckbox( self, L["Hide vendor values"], L["Hide vendor values, except while interacting with a vendor."] )
-	checkValue:SetPoint( "TOPLEFT", checkSoulbound, "BOTTOMLEFT", 0, -8 )
+	local checkBuy = CreateCheckbox(self, L["Hide buying instructions"])
+	checkBuy:SetPoint("TOPLEFT", checkSoulbound, "BOTTOMLEFT", 0, -8)
+	checkBuy.OnClick = OnClick
+	checkBuy.key = "hideRightClickBuy"
+
+	local checkSocket = CreateCheckbox(self, L["Hide socketing instructions"])
+	checkSocket:SetPoint("TOPLEFT", checkBuy, "BOTTOMLEFT", 0, -8)
+	checkSocket.OnClick = OnClick
+	checkSocket.key = "hideRightClickSocket"
+
+	local checkReqs = CreateCheckbox(self, L["Hide requirements"], L["Hide level, reputation, and skill requirements for items, enchantements, and sockets."])
+	checkReqs:SetPoint("TOPLEFT", checkSocket, "BOTTOMLEFT", 0, -8)
+	checkReqs.OnClick = OnClick
+	checkReqs.key = "hideRequirements"
+
+	local checkValue = CreateCheckbox(self, L["Hide vendor values"], L["Hide vendor values, except while interacting with a vendor."])
+	checkValue:SetPoint("TOPLEFT", checkReqs, "BOTTOMLEFT", 0, -8)
 	checkValue.OnClick = OnClick
 	checkValue.key = "hideSellValue"
 
-	self.refresh = function( self )
-		checkBonus:SetChecked( settings.compactBonuses )
-		checkILevel:SetChecked( settings.hideItemLevel )
-		checkBuy:SetChecked( settings.hideRightClickBuy )
-		checkSocket:SetChecked( settings.hideRightClickSocket )
-		checkMadeBy:SetChecked( settings.hideMadeBy )
-		checkSoulbound:SetChecked( settings.hideSoulbound )
-		checkValue:SetChecked( settings.hideSellValue )
+	self.refresh = function(self)
+		checkBonus:SetChecked(settings.compactBonuses)
+		checkILevel:SetChecked(settings.hideItemLevel)
+		checkEquipSets:SetChecked(GetCVarBool("dontShowEquipmentSetsOnItems"))
+		checkHeroic:SetChecked(settings.hideHeroic)
+		checkMadeBy:SetChecked(settings.hideMadeBy)
+		checkRaidFinder:SetChecked(settings.hideRaidFinder)
+		checkReforged:SetChecked(settings.hideReforged)
+		checkSoulbound:SetChecked(settings.hideSoulbound)
+		checkBuy:SetChecked(settings.hideRightClickBuy)
+		checkSocket:SetChecked(settings.hideRightClickSocket)
+		checkReqs:SetChecked(settings.hideRequirements)
+		checkValue:SetChecked(settings.hideSellValue)
 	end
 end
 
-local about = LibStub( "LibAboutPanel" ).new( ADDON_NAME, ADDON_NAME )
+local about = LibStub("LibAboutPanel").new(ADDON_NAME, ADDON_NAME)
 
 SLASH_ITEMTOOLTIPCLEANER1 = "/itc"
 
 SlashCmdList["ITEMTOOLTIPCLEANER"] = function()
-	InterfaceOptionsFrame_OpenToCategory( about )
-	InterfaceOptionsFrame_OpenToCategory( panel )
+	InterfaceOptionsFrame_OpenToCategory(about)
+	InterfaceOptionsFrame_OpenToCategory(panel)
 end
