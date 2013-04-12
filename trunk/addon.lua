96a16cc6c2e9b70487e4d1a80f80dccdd7e18c0d
Phanx-28751 <Phanx-28751@9955a2e6-1182-4174-8d3c-74d221240c8d>
2013-04-12 07:21:18 +0000

    - Fix saved variables - Fix reforge modification in zhCN - Add option to hide set bonuses - Add option to hide set item list - Add string caching

--- trunk/addon.lua
+++ trunk/addon.lua
@@ -9,24 +9,27 @@
 
 local ADDON_NAME, namespace = ...
 local L = namespace.L
+local db
 
-local settings = {
+local defaults = {
 	bonusColor = { 0, 1, 0 },
 	enchantColor = { 0, 0.8, 1 },
 	reforgeColor = { 1, 0.5, 1 },
 	hideBlank = true,
---	hideDurability = false,
 	hideEquipmentSets = true,
---	hideHeroic = false,
 	hideItemLevel = true,
 	hideMadeBy = true,
---	hideRaidFinder = false,
 	hideReforged = true,
 	hideRequirements = true,
 	hideRequirementsMet = true,
 	hideRightClickBuy = true,
 	hideRightClickSocket = true,
+--	hideDurability = false,
+--	hideHeroic = false,
+--	hideRaidFinder = false,
 --	hideSellValue = false,
+--	hideSetBonuses = false,
+--	hideSetItems = false,
 --	hideSoulbound = false,
 --	hideTransmog = false,
 --	hideTransmogLabel = false,
@@ -50,21 +53,33 @@ local ITEM_VENDOR_STACK_BUY  = ITEM_VENDOR_STACK_BUY
 local RAID_FINDER      = RAID_FINDER
 local REFORGED         = REFORGED
 
-local S_DURABILITY      = "^" .. gsub(DURABILITY_TEMPLATE, "%%d", "%%d+")
+local function topattern(str, plain)
+	str = gsub(str, "%%%d?$?c", ".+")
+	str = gsub(str, "%%%d?$?d", "%%d+")
+	str = gsub(str, "%%%d?$?s", ".+")
+	str = gsub(str, "([%(%)])", "%%%1")
+	return plain and str or ("^" .. str)
+end
+
+local S_DURABILITY      = topattern(DURABILITY_TEMPLATE)
 local S_ENCHANTED       = "^" .. gsub(ENCHANTED_TOOLTIP_LINE, "%%s", "(.+)")
-local S_ITEM_LEVEL      = "^" .. gsub(ITEM_LEVEL, "%%%d?$?d", "%%d+")
-local S_MADE_BY         = "^" .. gsub(ITEM_CREATED_BY, "%%%d?$?s", ".+")
-local S_REFORGED        = "^(" .. gsub(strsub(REFORGE_TOOLTIP_LINE, 1, 7), "%%[cs]", ".-") .. ")" .. gsub(gsub(strsub(REFORGE_TOOLTIP_LINE, 8), "%%s", ".-"), "[%(%)]", "%%%1")
-local S_REQ_CLASS       = "^" .. gsub(ITEM_CLASSES_ALLOWED, "%%s", ".+")
-local S_REQ_LEVEL       = "^" .. gsub(ITEM_MIN_LEVEL, "%%%d?$?d", "%%d+")
-local S_REQ_RACE        = "^" .. gsub(ITEM_RACES_ALLOWED, "%%s", ".+")
-local S_REQ_REPUTATION  = "^" .. gsub(ITEM_REQ_REPUTATION, "%%%d?$?s", ".+")
-local S_REQ_SKILL       = "^" .. gsub(ITEM_REQ_SKILL, "%%%d?$?s", ".+")
-local S_TRANSMOGRIFIED  = "^" .. gsub(TRANSMOGRIFIED, "%%s", "(.+)")
-local S_UNIQUE_MULTIPLE = "^" .. gsub(ITEM_UNIQUE_MULTIPLE, "%%d", "%%d+")
-local S_UPGRADE_LEVEL   = "^" .. gsub(ITEM_UPGRADE_TOOLTIP_FORMAT, "%%d", "%%d+")
+local S_ITEM_LEVEL      = topattern(ITEM_LEVEL)
+local S_ITEM_SET_BONUS  = topattern(ITEM_SET_BONUS)
+local S_ITEM_SET_BONUS_GRAY = topattern(ITEM_SET_BONUS_GRAY)
+local S_ITEM_SET_NAME   = topattern(ITEM_SET_NAME)
+local S_MADE_BY         = topattern(ITEM_CREATED_BY)
+local S_REFORGED        = "^(.+)" .. topattern(strsub(REFORGE_TOOLTIP_LINE, (strfind(REFORGE_TOOLTIP_LINE, " ?[%(（]"))), true)
+local S_REQ_CLASS       = topattern(ITEM_CLASSES_ALLOWED)
+local S_REQ_LEVEL       = topattern(ITEM_MIN_LEVEL)
+local S_REQ_RACE        = topattern(ITEM_RACES_ALLOWED)
+local S_REQ_REPUTATION  = topattern(ITEM_REQ_REPUTATION)
+local S_REQ_SKILL       = topattern(ITEM_REQ_SKILL)
+local S_TRANSMOGRIFIED  = topattern(TRANSMOGRIFIED)
+local S_UNIQUE_MULTIPLE = topattern(ITEM_UNIQUE_MULTIPLE)
+local S_UPGRADE_LEVEL   = topattern(ITEM_UPGRADE_TOOLTIP_FORMAT)
 
 local cache = setmetatable({ }, { __mode = "kv" }) -- weak table to enable garbage collection
+namespace.cache = cache -- so it can be wiped when an option changes
 
 local lines = setmetatable({ }, { __index = function(lines, tooltip)
 	local lines_tooltip = setmetatable({ name = tooltip:GetName() }, { __index = function(lines_tooltip, line)
@@ -76,83 +91,129 @@ local lines = setmetatable({ }, { __index = function(lines, tooltip)
 	return lines_tooltip
 end })
 
+local inSetList
+
+local function ReformatLine(tooltip, line, text)
+	if text == " " and db.hideBlank then
+		if tooltip.shownMoneyFrames then
+			for j = 1, tooltip.shownMoneyFrames do
+				if select(2,_G[tooltip:GetName().."MoneyFrame"..j]:GetPoint("LEFT")) == line then
+					return
+				end
+			end
+		end
+		-- can't cache this because it breaks the money display
+		line:SetText("")
+	return end
+
+	if cache[text] then
+		-- can't cache blank lines so check that first
+		line:SetText(cache[text])
+	return end
+
+	if strmatch(text, S_ENCHANTED) then
+		-- no cache for colors yet
+		line:SetText(strmatch(text, S_ENCHANTED))
+		line:SetTextColor(unpack(db.enchantColor))
+	return end
+
+	if strmatch(text, S_REFORGED) then
+		-- no cache for colors yet
+		line:SetTextColor(unpack(db.reforgeColor))
+		if db.hideReforged then
+			line:SetText(strmatch(text, S_REFORGED))
+		end
+	return end
+
+	if db.hideTransmog then
+		local new = strmatch(text, S_TRANSMOGRIFIED)
+		if new then
+			if db.hideTransmogLabelOnly then
+				local new = strmatch(text, S_TRANSMOGRIFIED)
+				cache[text] = new
+				return line:SetText(new)
+			else
+				cache[text] = ""
+				return line:SetText("")
+			end
+		return end
+	end
+
+	if inSetList then
+		if strmatch(text, "^ ") then
+			--print("Found set item:", text)
+			if db.hideSetItems then
+				cache[text] = ""
+				line:SetText("")
+			end
+			return
+		else
+			--print("End of set list:", text)
+			inSetList = nil
+		end
+	elseif strmatch(text, S_ITEM_SET_NAME) then
+		--print("Found set name:", text)
+		inSetList = true
+	end
+
+	if (text == ITEM_HEROIC and db.hideHeroic)
+	or (text == ITEM_SOCKETABLE and db.hideRightClickSocket)
+	or (text == ITEM_SOULBOUND and db.hideSoulbound)
+	or (text == ITEM_VENDOR_STACK_BUY and db.hideRightClickBuy)
+	or (text == REFORGED and db.hideReforged)
+	or (text == RAID_FINDER and db.hideRaidFinder)
+	or (db.hideDurability and strmatch(text, S_DURABILITY))
+	or (db.hideItemLevel and strmatch(text, S_ITEM_LEVEL))
+	or (db.hideMadeBy and strmatch(text, S_MADE_BY))
+	or (db.hideUpgradeLevel and strmatch(text, S_UPGRADE_LEVEL))
+	or (db.hideUnique and (text == ITEM_UNIQUE or text == ITEM_UNIQUE_EQUIPPABLE or strmatch(text, S_UNIQUE_MULTIPLE)))
+	or (db.hideSetBonuses and (strmatch(text, S_ITEM_SET_BONUS) or strmatch(text, S_ITEM_SET_BONUS_GRAY)))
+	then
+		cache[text] = ""
+		line:SetText("")
+	return end
+
+	if db.hideRequirements and (
+		strmatch(text, S_REQ_CLASS)
+		or strmatch(text, S_REQ_RACE)
+		or strmatch(text, S_REQ_LEVEL)
+		or strmatch(text, S_REQ_REPUTATION)
+		or strmatch(text, S_REQ_SKILL)
+		or strmatch(text, L.ENCHANT_REQUIRES)
+		or strmatch(text, L.SOCKET_REQUIRES)
+	) then
+		if db.hideRequirementsMet then
+			-- hide only if met
+			local r, g, b = line:GetTextColor()
+			if g > 0.9 and b > 0.9 then
+				cache[text] = ""
+				line:SetText("")
+			end
+		else
+			cache[text] = ""
+			line:SetText("")
+		end
+	return end
+
+	if strmatch(text, "^%+%d+") then
+		-- no cache for colors yet
+		local r, g, b = line:GetTextColor()
+		if r < 0.1 and g > 0.9 and b < 0.1 then
+			line:SetTextColor(unpack(db.bonusColor))
+		end
+	return end
+end
+
 local function ReformatItemTooltip(tooltip)
 	local tooltipName = tooltip:GetName()
 	for i = 2, tooltip:NumLines() do
 		local line = _G[tooltipName .. "TextLeft" .. i]
 		local text = line:GetText()
 		if text then
-			if strmatch(text, S_ENCHANTED) then
-				line:SetText(strmatch(text, S_ENCHANTED))
-				line:SetTextColor(unpack(settings.enchantColor))
-
-			elseif strmatch(text, S_REFORGED) then
-				line:SetTextColor(unpack(settings.reforgeColor))
-				if settings.hideReforged then
-					line:SetText(strmatch(text, S_REFORGED))
-				end
-
-			elseif settings.hideTransmog and strmatch(text, S_TRANSMOGRIFIED) then
-				if settings.hideTransmogLabelOnly then
-					line:SetText(strmatch(text, S_TRANSMOGRIFIED))
-				else
-					line:SetText("")
-				end
-
-			elseif settings.hideRequirements and
-			(strmatch(text, S_REQ_CLASS)
-				or strmatch(text, S_REQ_RACE)
-				or strmatch(text, S_REQ_LEVEL)
-				or strmatch(text, S_REQ_REPUTATION)
-				or strmatch(text, S_REQ_SKILL)
-				or strmatch(text, L.ENCHANT_REQUIRES)
-				or strmatch(text, L.SOCKET_REQUIRES)
-			) then
-				if settings.hideRequirementsMet then
-					-- hide only if met
-					local r, g, b = line:GetTextColor()
-					if g > 0.9 and b > 0.9 then
-						line:SetText("")
-					end
-				else
-					line:SetText()
-				end
-
-			elseif (text == " " and settings.hideBlank) then
-				local isAnchor
-				if tooltip.shownMoneyFrames then
-					for j = 1, tooltip.shownMoneyFrames do
-						if select(2,_G[tooltip:GetName().."MoneyFrame"..j]:GetPoint("LEFT")) == line then
-							isAnchor = true
-							break
-						end
-					end
-				end
-				if not isAnchor then
-					line:SetText("")
-				end
-
-			elseif (text == ITEM_HEROIC and settings.hideHeroic)
-			or (text == ITEM_SOCKETABLE and settings.hideRightClickSocket)
-			or (text == ITEM_SOULBOUND and settings.hideSoulbound)
-			or (text == ITEM_VENDOR_STACK_BUY and settings.hideRightClickBuy)
-			or (text == REFORGED and settings.hideReforged)
-			or (text == RAID_FINDER and settings.hideRaidFinder)
-			or (settings.hideDurability and strmatch(text, S_DURABILITY))
-			or (settings.hideItemLevel and strmatch(text, S_ITEM_LEVEL))
-			or (settings.hideMadeBy and strmatch(text, S_MADE_BY))
-			or (settings.hideUpgradeLevel and strmatch(text, S_UPGRADE_LEVEL))
-			or (settings.hideUnique and (text == ITEM_UNIQUE or text == ITEM_UNIQUE_EQUIPPABLE or strmatch(text, S_UNIQUE_MULTIPLE))) then
-				line:SetText("")
-
-			elseif strmatch(text, "^%+%d+") then
-				local r, g, b = line:GetTextColor()
-				if r < 0.1 and g > 0.9 and b < 0.1 then
-					line:SetTextColor(unpack(settings.bonusColor))
-				end
-			end
+			ReformatLine(tooltip, line, text)
 		end
 	end
+	inSetList = nil
 	tooltip:Show()
 end
 
@@ -175,22 +236,22 @@ local itemTooltips = {
 	"AtlasLootTooltipTEMP",
 }
 
-local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", function(f, e)
+local Loader = CreateFrame("Frame")
+Loader:RegisterEvent("ADDON_LOADED")
+Loader:SetScript("OnEvent", function(self, event, arg)
 	-- Initialize addon settings:
-	if addon == ADDON_NAME then
-		if ItemTooltipCleanerSettings then
-			for k, v in pairs(settings) do
-				if type(v) ~= type(ItemTooltipCleanerSettings[k]) then
-					ItemTooltipCleanerSettings[k] = v
-				end
-			end
-			for k, v in pairs(ItemTooltipCleanerSettings) do
-				settings[k] = v
+	if arg == ADDON_NAME then
+		if not ItemTooltipCleanerSettings then
+			ItemTooltipCleanerSettings = {}
+		end
+		db = ItemTooltipCleanerSettings
+		--[[
+		for k, v in pairs(defaults) do
+			if type(db[k]) ~= type(v) then
+				db[k] = v
 			end
 		end
-		ItemTooltipCleanerSettings = settings
+		]]
 	end
 
 	-- Hook tooltips:
@@ -201,8 +262,8 @@ f:SetScript("OnEvent", function(f, e)
 		end
 	end
 	if not next(itemTooltips) then
-		f:UnregisterEvent(e)
-		f:SetScript("OnEvent", nil)
+		self:UnregisterEvent(event)
+		self:SetScript("OnEvent", nil)
 	end
 end)
 
@@ -218,7 +279,7 @@ local showPriceFrames = {
 local prehook = GameTooltip_OnTooltipAddMoney
 
 function GameTooltip_OnTooltipAddMoney(...)
-	if not settings.hideSellValue then
+	if not db.hideSellValue then
 		return prehook(...)
 	end
 	for _, name in pairs(showPriceFrames) do
